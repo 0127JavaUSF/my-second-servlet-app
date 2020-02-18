3240ea3db02b37cc4992ca0b2c6c3c8dc256df81
Mitch Goshorn <msgoshorn@gmail.com>
2020-02-18 16:18:01 -0500

    initial setup, basic servlet configuration with logic supplied to respond to a post request

---
+++ src/main/java/com/revature/servlets/AServletOfSomeSort.java
@@ -0,0 +1,17 @@
+package com.revature.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AServletOfSomeSort extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request, 
+			HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().write("Hello of some sort!");
+	}
+}
